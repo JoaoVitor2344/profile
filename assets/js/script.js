95a5c8c62e95495a8aaef44b90f350d0839684af
João Vitor <joaovitorb2345@gmail.com>
2022-10-20 22:27:20 -0300

    1.0?

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$("#menu").load("/menu.html");
+
+$("#btn-verificar" ).on("click", function() {
+  window.location.href="proposta.html";
+});
+
+$("#nome").on("click", function() {
+   window.location.href="index.html"; 
+})
